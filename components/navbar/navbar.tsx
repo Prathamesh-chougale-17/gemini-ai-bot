135c2de9c1b74b7b4ba72ad4cc2c09ad63c55592
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-03 11:12:38 +0530

    Remove unused import and component from navbar.tsx

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,7 +1,4 @@
 import { NavbarRoutes } from "@/components/navbar-routes";
-
-import { MobileSidebar } from "./mobile-sidebar";
-
 export const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex z-40 bg-white dark:bg-black items-centershadow-sm">

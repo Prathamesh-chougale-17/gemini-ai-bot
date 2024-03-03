f1b47ed0468a8f3ca4db24fc8d879bdefa50fe2d
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-03-03 11:12:24 +0530

    Remove MobileSidebar from Navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,7 +5,6 @@ import { MobileSidebar } from "./mobile-sidebar";
 export const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex z-40 bg-white dark:bg-black items-centershadow-sm">
-      <MobileSidebar />
       <NavbarRoutes />
     </div>
   );

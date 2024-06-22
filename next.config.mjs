538d7a0e17349a0ba808e4b5ab4492b67e33acbf
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-06-22 10:58:39 +0530

    chore: Update next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,4 @@
-import process from 'node:process';
-Object.assign(process.env, { NEXT_TELEMETRY_DISABLED: '1' });
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-/**
- * @typedef {import('next').NextConfig} NextConfig
- * @typedef {Array<((config: NextConfig) => NextConfig)>} NextConfigPlugins
- */
-import nextMDX from '@next/mdx';
-import rehypeSlug from 'rehype-slug';
-import rehypePrettyCode from 'rehype-pretty-code';
-import moonlightTheme from './assets/moonlight-ii.json' with { type: 'json' };
-
-/** @type {NextConfigPlugins} */
-const plugins = [];
-
-/** @type {NextConfig} */
-const nextConfig = {
-  cleanDistDir: true,
-  reactStrictMode: true,
-  poweredByHeader: false,
-  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
-  env: {
-    NEXT_TELEMETRY_DISABLED: '1',
-  },
-};
-
-/** @type {import('rehype-pretty-code').Options} */
-const options = {
-  keepBackground: false,
-  theme: moonlightTheme,
-};
-
-plugins.push(
-  nextMDX({
-    extension: /\.(md|mdx)$/,
-    options: {
-      remarkPlugins: [],
-      rehypePlugins: [[rehypePrettyCode, options], rehypeSlug],
-    },
-  }),
-);
-
-export default () => plugins.reduce((_, plugin) => plugin(_), nextConfig);
+export default nextConfig;
